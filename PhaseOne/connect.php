d7eef00941884958c31d89bba710955bda014b6b
Andrew Clark <r00@live.ca>
2020-07-31 20:31:03 -0400

    Update connect.php

--- PhaseOne/connect.php
+++ PhaseOne/connect.php
@@ -1,21 +1,15 @@
 <!-- Connect to the New Database  -->
 
 
-<?php $dsn = 'mysql:host=172.31.22.43; dbname=Andrew100073101';
-$username = 'Andrew100073101';
-$password = 'LhlSP8hk1P';
+<?php $dsn = 'mysql:host=localhost;dbname=phase2';
+$username = 'root';
+$password = '';
 $db = new PDO($dsn, $username, $password);
 //set error mode to exception 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 ?>
 
 
 
 
 
-<!-- $dsn = 'mysql:host=localhost;dbname=phase2';
-$username = 'root';
-$password = '';
-$db = new PDO($dsn, $username, $password);
-//set error mode to exception 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); -->
